3b9199dd823f2eb7ee5325b3ab2d28e4cd5aeca5
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-05-31 12:46:49 +0530

    fun commands and bot activity

---
+++ cogs/fun.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import random
+import requests
+from PIL import Image
+import re
+
+class Fun(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def ping(self, ctx):
+        embed = discord.Embed(title=f':ping_pong: Pong! ({round(self.client.latency * 1000)} ms)', colour=0x00ff00)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def emojify(self, ctx, *, text):
+        emoji_string = ""
+        for char in text:
+            if re.match(r'[a-zA-Z]', char):
+                emoji = f":regional_indicator_{char}:"
+                emoji_string += emoji
+            else:
+                emoji_string += char
+        await ctx.send(emoji_string)
+
+def setup(client):
+    client.add_cog(Fun(client))
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 if os.path.isfile('.env'):
     load_dotenv()
 
-cogs = ['cogs.admin', 'cogs.roles', 'cogs.logger', 'cogs.error_handler']
+cogs = ['cogs.admin', 'cogs.roles', 'cogs.logger', 'cogs.error_handler', 'cogs.fun']
 
 client = commands.Bot(command_prefix=os.getenv('PREFIX'), guild_subscriptions=True)
 client.load_extension('jishaku')
@@ -19,6 +19,11 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
+@client.event
+async def on_connect():
+    activity = discord.Activity(name=os.getenv('ACTIVITY'), type=discord.ActivityType.listening)
+    await client.change_presence(activity=activity)
 
 if __name__ == '__main__':
     client.run(os.getenv('TOKEN'))
